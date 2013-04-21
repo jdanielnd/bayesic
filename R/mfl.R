0cc1c3b2810428c5cf6d939cf1ccaa4da67a3f00
João Daniel <jdanielnd@gmail.com>
2013-04-21 13:38:37 -0300

    adding norm vs

--- R/mfl.R
+++ R/mfl.R
@@ -10,16 +10,16 @@ treatbm <- function(mats) {
   res 
 }
 
-mflist <- function(mats,pca) {
+mflist <- function(mats,pca,vs) {
   mats.treated <- treat.data(mats,pca)
   lapply(mats.treated, function(x) {
-    mfl(x)
+    mfl(x,vs)
   }) 
 }
 
-mfl <- function(mat) {
+mfl <- function(mat,vs) {
 	# res <- mflC(1000,mat)
-  res <- gibbs.mfle(mat)
+  res <- gibbs.mfle(mat,vs=vs)
 	pmat <- res[[5]]
 	pvec <- apply(pmat,2,mean)
 	pvec
@@ -37,7 +37,7 @@ mflC <- function(ite, x) {
   .Call("mfl", ite, x, PACKAGE="bayesic")
 }
 
-gibbs.mfle <- function(x, ite=1000, omega=10, a=2.1, b=1.1, gamma_a=1, gamma_b=1) {
+gibbs.mfle <- function(x, vs=c("dirak","norm"), ite=1000, omega=10, omega_1=omega/1000, a=2.1, b=1.1, gamma_a=1, gamma_b=1) {
 
   alpha <- array(0.1,c(nrow(x),1))
   sigma2 <- array(1,c(nrow(x),1))
@@ -69,17 +69,34 @@ gibbs.mfle <- function(x, ite=1000, omega=10, a=2.1, b=1.1, gamma_a=1, gamma_b=1
       gamma_2 <- gamma_b + 1 - z[i]
       p <- rbeta(1, gamma_1, gamma_2)
       
-      # alpha sampling
-      v_alpha <- 1/((1/omega) + (sum(lambda^2)/sigma2[i]))
-      m_alpha <- v_alpha*(sum(x[i,]*lambda)/sigma2[i])
-      
-      if(z[i]) {
-        alpha[i] <- rnorm(1,m_alpha,sqrt(v_alpha))
+      if(vs=="norm") {
+        # alpha sampling
+        v_alpha_0 <- 1/((1/omega) + (sum(lambda^2)/sigma2[i]))
+        m_alpha_0 <- v_alpha_0*(sum(x[i,]*lambda)/sigma2[i])
+        v_alpha_1 <- 1/((1/omega_1) + (sum(lambda^2)/sigma2[i]))
+        m_alpha_1 <- v_alpha_1*(sum(x[i,]*lambda)/sigma2[i])
+        
+        if(z[i]) {
+          alpha[i] <- rnorm(1,m_alpha_0,sqrt(v_alpha_0))
+        } else {
+          alpha[i] <- rnorm(1,m_alpha_1,sqrt(v_alpha_1))
+        }
+        frac_1 = dnorm(0,m_alpha_0,sqrt(v_alpha_0))/dnorm(0,0,sqrt(omega))
+        frac_2 = dnorm(0,0,sqrt(omega_1))/dnorm(0,m_alpha_1,sqrt(v_alpha_1))
+        p_star[i] <- p/(p + frac_1*frac_2*(1-p))
       } else {
-        alpha[i] <- 0
+        # alpha sampling
+        v_alpha <- 1/((1/omega) + (sum(lambda^2)/sigma2[i]))
+        m_alpha <- v_alpha*(sum(x[i,]*lambda)/sigma2[i])
+        
+        if(z[i]) {
+          alpha[i] <- rnorm(1,m_alpha,sqrt(v_alpha))
+        } else {
+          alpha[i] <- 0
+        }
+        p_star[i] <- p/(p + (dnorm(0,m_alpha,sqrt(v_alpha))/dnorm(0,0,sqrt(omega)))*(1-p))
       }
 
-      p_star[i] <- p/(p + (dnorm(0,m_alpha,sqrt(v_alpha))/dnorm(0,0,sqrt(omega)))*(1-p))
     }
     # Construindo a matrix diagonal com os novos sigma2
     for(i in 1:m){Dinv[i,i] = 1/sigma2[i]}
